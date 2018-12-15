1956bc9d413f499c438ef747816e90b39895459c
Sigbjorn Bore <sigbjorn.loland.bore@gmail.com>
2018-12-15 15:17:17 +0100

    new

--- DNA_ANALYSIS_CODE/ana_struc.py
+++
@@ -1,9 +0,0 @@
-import sys
-import numpy as np
-A=np.loadtxt(sys.argv[1])
-print "rise/bp = %.3f+/-%.2f nm"%(0.1*np.mean(A[:,1:2]),0.1*np.std(A[:,1:2]))
-print "radius = %.3f+/-%.2f nm"%(0.1*np.mean(A[:,3:4]),0.1*np.std(A[:,3:4]))
-print "diameter = %.2f+/-%.2f nm"%(2*0.1*np.mean(A[:,3:4]),2*0.1*np.std(A[:,3:4]))
-print "ang/bp = %.1f+/-%.1f deg"%(np.mean(A[:,5:6]),np.std(A[:,5:6]))
-print "Bases per turn =",360./np.mean(A[:,5:6]),'+/-',360./np.mean(A[:,5:6])**2*np.std(A[:,5:6])
-

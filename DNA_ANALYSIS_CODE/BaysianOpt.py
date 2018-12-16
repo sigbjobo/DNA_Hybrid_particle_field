d88bf10aff8bea2297c7940cba92a63a3fbe4390
Sigbjørn Løland Bore <s.l.bore@kjemi.uio.no>
2018-12-16 13:54:03 +0100

    a

---
+++ DNA_ANALYSIS_CODE/BaysianOpt.py
@@ -0,0 +1,36 @@
+import numpy as np
+from gpflowopt.domain import ContinuousParameter
+import FUNC as F
+
+
+from gpflowopt.domain import ContinuousParameter
+#import FUNC as FUNC                                                                                                                                                         
+domain = ContinuousParameter('x1', -5, 30) + \
+         ContinuousParameter('x2', -30, 5)
+
+import gpflow
+from gpflowopt.bo import BayesianOptimizer
+from gpflowopt.design import LatinHyperCube
+from gpflowopt.acquisition import ExpectedImprovement
+from gpflowopt.optim import SciPyOptimizer, StagedOptimizer, MCOptimizer
+
+# Use standard Gaussian process Regression
+data=np.loadtxt("XY.dat")
+#X=LatinHyperCube(10, domain)
+#print(X)
+X = data[:,:2]
+#print(X)
+Y =  data[:,-1][:,None]
+model = gpflow.gpr.GPR(X, Y, gpflow.kernels.Matern52(2, ARD=True))
+model.kern.lengthscales.transform = gpflow.transforms.Log1pe(1e-3)
+
+# Now create the Bayesian Optimizer
+alpha = ExpectedImprovement(model)
+acquisition_opt = StagedOptimizer([MCOptimizer(domain, 200),
+                                   SciPyOptimizer(domain)])
+
+optimizer = BayesianOptimizer(domain, alpha, optimizer=acquisition_opt, verbose=True)
+
+# Run the Bayesian optimization
+r = optimizer.optimize(F.func, n_iter=10)
+ 
